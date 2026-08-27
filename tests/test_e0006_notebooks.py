7f6d258918f3715fa168c05fef532bec78ebaf13
SirSpin <pmartinsjr87@gmail.com>
2026-08-27 09:49:39 -0500

    Compile-check exact CUDA 12.9 resolution probe

--- tests/test_e0006_notebooks.py
+++ tests/test_e0006_notebooks.py
@@ -9,6 +9,7 @@ NOTEBOOKS = (
     Path("notebooks/E0006_lightning_gate_b_kaggle.ipynb"),
     Path("notebooks/E0006_mirror_nvfp4_to_kaggle.ipynb"),
     Path("notebooks/E0006_vllm_dependency_probe_kaggle.ipynb"),
+    Path("notebooks/E0006_vllm_cu129_resolution_probe_kaggle.ipynb"),
 )
 
 
