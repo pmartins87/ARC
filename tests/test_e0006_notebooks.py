a4ae5b95da360b7f59dcc26981a90cad5d8d36ab
SirSpin <pmartinsjr87@gmail.com>
2026-08-27 21:23:42 -0500

    Compile-check E0006 wheelhouse builder notebook

--- tests/test_e0006_notebooks.py
+++ tests/test_e0006_notebooks.py
@@ -10,6 +10,7 @@ NOTEBOOKS = (
     Path("notebooks/E0006_mirror_nvfp4_to_kaggle.ipynb"),
     Path("notebooks/E0006_vllm_dependency_probe_kaggle.ipynb"),
     Path("notebooks/E0006_vllm_cu129_resolution_probe_kaggle.ipynb"),
+    Path("notebooks/E0006_vllm_cu129_wheelhouse_builder_kaggle.ipynb"),
 )
 
 
